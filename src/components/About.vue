4bac05cdfa83b8759399dbf5170b104d37c1d7e1
fantasy1988 <dcqdcq1987@126.com>
2019-05-25 17:23:13 +0800

    项目创建

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>about</h1>
+        <p>{{aboutMsg}}</p>
+        <router-link to="/">Home</router-link>        
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                aboutMsg: '我是about组件'
+            }
+        }
+    }
+</script>
